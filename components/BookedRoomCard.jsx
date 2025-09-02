0c544720b9fe758b43c1ca517aad79c4ff228fca
avinashmaurya23 <82766958+avinashmaurya23@users.noreply.github.com>
2025-09-02 17:31:53 +0530

    Cancel after

--- components/BookedRoomCard.jsx
+++ components/BookedRoomCard.jsx
@@ -32,7 +32,7 @@ const StatusBadge = ({ status }) => {
 function BookedRoomCard({ booking }) {
   const { room_id: room } = booking;
   const isCancellable =
-    booking.booking_status === "Declined" ||
+    booking.booking_status === "Pending" ||
     booking.booking_status === "Confirm";
 
   return (
