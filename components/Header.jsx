7f49a837fbed18b4a9c475d57c2f328a31bba47c
avinashmaurya23 <82766958+avinashmaurya23@users.noreply.github.com>
2025-09-04 14:49:36 +0530

    Bookings to mY booking in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -86,7 +86,7 @@ const Header = () => {
                       href="/bookings"
                       className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                     >
-                      Bookings
+                      My Bookings
                     </Link>
                     {isAdmin && (
                       <>
